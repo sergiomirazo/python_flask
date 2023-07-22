31802ca4db3a130f4e703e77c627971e8aab246c
Sergio Mirazo <106106677+sergiomirazo@users.noreply.github.com>
2023-07-22 10:53:48 -0700

    Create products.py
    just a provisional json object database

---
+++ products.py
@@ -0,0 +1,5 @@
+products = [
+    {"name": "laptop", "price": 800, "quantity": 4, "image": "'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVCePVOZDrLLBQlzqAD55pwVM-isMnTZ0dLA'"},
+    {"name": "mouse", "price": 40, "quantity": 10, "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB5_aQ2OC8nSx2neAKCTjp9mL6sf1T5cqCBQ"},
+    {"name": "monitor", "price": 400, "quantity": 3, "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQskDxI3USNLPFYfTQGTnXapgFCiLXNB0Jr4w"}
+]
